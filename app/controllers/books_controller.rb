bcf7aa915e3f5a2d649440da3a4831b18e818c78
Yoshiteru Toyoda <6089more@gmail.com>
2021-07-08 23:28:03 +0000

    [Start]削除機能まで完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,13 +11,25 @@ class BooksController < ApplicationController
     redirect_to(book_path(@book.id))
   end
   
-  def new
-  end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+      @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(books_params)
+    redirect_to(book_path(book.id))
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to(books_path)
   end
   
 private
