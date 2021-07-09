5b9cecc430fa7d5afb4947b1a2920072c4f6620e
Yoshiteru Toyoda <6089more@gmail.com>
2021-07-09 12:30:09 +0000

    かいどく

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,20 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(books_params)
-    @book.save
-    redirect_to(book_path(@book.id))
+    if @book.save
+      redirect_to(book_path(@book.id))
+    else
+      render :index
+    end
+    flash[:notice] = "Book was successfully created."
   end
-  
+
 
   def show
     @book = Book.find(params[:id])
@@ -19,23 +23,31 @@ class BooksController < ApplicationController
   def edit
       @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(books_params)
-    redirect_to(book_path(book.id))
+   if book.save
+       redirect_to(book_path(book.id))
+  else
+      render :show
+   end
+
+    flash[:notice] = "Book was successfully updated."
+
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to(books_path)
+    flash[:notice] = "Book was successfully destroyed."
   end
-  
+
 private
-  
+
   def books_params
   params.require(:book).permit(:title,:body)
   end
-  
+
 end
